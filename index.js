f425a2275e221d35fa5ee8be381199a51618ebad
aditya jain <aj257453@gmail.com>
2026-01-03 18:56:22 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import { fileURLToPath } from "url";
 config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"]
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
