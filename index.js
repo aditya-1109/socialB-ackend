657bbf094bea23929bd3626f3d2c60013fc30f28
aditya jain <aj257453@gmail.com>
2026-01-03 22:38:16 +0530

    cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
   },
   methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  credentials: "include"
 }));
 
 
