27d60e158b9a4bfec99f23199ae23fe63ae1bb03
aditya jain <aj257453@gmail.com>
2026-01-03 19:16:33 +0530

    type

--- index.js
+++ index.js
@@ -10,23 +10,13 @@ config();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",                
-  "https://social-gules-nu.vercel.app"  
-];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: "*", 
   methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
-  credentials: true 
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
 
 
 app.use(express.json());
